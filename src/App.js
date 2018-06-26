d8baf3dd93151eeaa0510d73c25d4758eb4819c7
Lucas Rabuffetti <lucas.rabuffetti@gmail.com>
2018-06-26 12:39:03 +0200

    hashRouter added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'
 import AppHolder from './components/appHolder/AppHolder';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router>
+      <HashRouter>
         <AppHolder />
-      </Router>
+      </HashRouter>
     );
   }
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import NavItem from '../navItem/NavItem'
 
 class Header extends Component {
@@ -33,12 +34,12 @@ class Header extends Component {
         return (
             <header>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
-                    <a className="navbar-brand js-scroll-trigger" href="/">
+                    <Link className="navbar-brand js-scroll-trigger" to='/'>
                         <span className="d-block d-lg-none">Lucas Rabuffetti CV</span>
                         <span className="d-none d-lg-block">
                             <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={require('../../assets/img/profile.png')} alt="" />
                         </span>
-                    </a>
+                    </Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
