8928d2cafa2ec2ae43f9ad6b76ed4587b1448be4
Lucas Rabuffetti <lucas.rabuffetti@gmail.com>
2018-06-26 14:26:20 +0200

    BrowserRouter approach

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import AppHolder from './components/appHolder/AppHolder';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <HashRouter>
+      <BrowserRouter>
         <AppHolder />
-      </HashRouter>
+      </BrowserRouter>
     );
   }
 }
