40fb6f1bc37eb489bfd51c2d3758a59e3184122d
Lucas Rabuffetti <lucas.rabuffetti@gmail.com>
2018-06-30 15:56:15 +0200

    gamma of colors changed

--- src/components/mainContent/MainContent.js
+++ src/components/mainContent/MainContent.js
@@ -9,7 +9,7 @@ import About from '../about/About';
 class MainContent extends React.Component {
   render() {
     return (
-      <div className="container-fluid p-0">
+      <div className="container-fluid p-0 section-bg">
         <Switch>
           <Route exact path={'/'} component={Home} />
           <Route exact path={'/lucas-rabuffetti/experience'} component={Experience} />
